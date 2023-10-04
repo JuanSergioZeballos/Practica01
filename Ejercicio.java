6a2698254253e1da3bc0ce62377751d17bcb94bd
rcompanocca <ronicompanocca@gmail.com>
2023-10-04 11:56:47 -0500

    PRACTICA

--- Ejercicio.java
+++ Ejercicio.java
@@ -1,3 +1,92 @@
+import java.io.*;
+import java.util.Scanner;
+
 public class Ejercicio {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Student[] students = null;
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader("Datos.csv"));
+            String line = null;
+            int count = 0;
+
+            // Contar las líneas en el archivo para determinar el tamaño del array
+            while ((line = reader.readLine()) != null) {
+                count++;
+            }
+
+            // Reiniciar el lector para volver al principio del archivo
+            reader.close();
+            reader = new BufferedReader(new FileReader("Datos.csv"));
+
+            students = new Student[count];
+            int index = 0;
+
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(";");
+                Student estudiante = new Student(
+                    parts[0], parts[1], parts[2], parts[3], parts[4], Integer.parseInt(parts[5]), parts[6], parts[7]
+                );
+                students[index] = estudiante;
+                index++;
+            }
+
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (students != null) {
+            for (int i = 0; i < students.length; i++) {
+                System.out.println(students[i].toString());
+            }
+
+            System.out.println("==========================");
+            System.out.println("==========================");
+            System.out.println("==========================");
+            System.out.println("Ingrese el nombre del alumno a buscar: ");
+            String nombre = sc.next();
+            int pos = busquedaSecuencialNombre(students, nombre);
+
+            if (pos != -1) {
+                System.out.println(students[pos].toString());
+            } else {
+                System.out.println("No encontrado");
+            }
+        }
+        // Ordenar estudiantes por edad usando el algoritmo de ordenamiento de burbuja
+        ordenarPorEdad(students);
+
+        // Imprimir estudiantes ordenados por edad
+        System.out.println("Estudiantes ordenados por edad:");
+        for (int i = 0; i < students.length; i++) {
+            System.out.println(students[i].toString());
+        }
+    }
+
+    public static int busquedaSecuencialNombre(Student[] students, String s) {
+        for (int i = 0; i < students.length; i++) {
+            if (s.equals(students[i].getName())) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void ordenarPorEdad(Student[] students) {
+        int n = students.length;
+        boolean intercambio;
+        do {
+            intercambio = false;
+            for (int i = 0; i < n - 1; i++) {
+                if (students[i].getEdad() > students[i + 1].getEdad()) {
+                    // Intercambiar estudiantes
+                    Student temp = students[i];
+                    students[i] = students[i + 1];
+                    students[i + 1] = temp;
+                    intercambio = true;
+                }
+            }
+        } while (intercambio);
+    }
 }
--- Student.java
+++ Student.java
@@ -1,35 +1,100 @@
-// FUNDAMENTOS DE PROGRAMACION 2 - PRACTICA 01
-import java.util.Scanner;
+// Creamos la clase Student, para gurdar los datos de los estudiantes
+public class Student {
+    private String Name;
+    private String Materno;
+    private String Paterno;
+    private String Gender;
+    private String CUI;
+    private String Status;
+    private String Email;
+    private String Edad;
 
-public class Student{
-	String nombre,apellido;
+    // Constructor
+    public Student(String CUI, String Email, String Name, String Paterno, String Materno, String Edad, String Status, String Gender) {
+        this.Name = Name;
+        this.Materno = Materno;
+        this.Paterno = Paterno;
+        this.Gender = Gender;
+        this.CUI = CUI;
+        this.Status = Status;
+        this.Email = Email;
+        this.Edad = Edad;
+    }
+
+    // Getters
+    public String getName() {
+        return Name;
+    }
+
+    public String getMaterno() {
+        return Materno;
+    }
+
+    public String getPaterno() {
+        return Paterno;
+    }
+
+    public String getGender() {
+        return Gender;
+    }
+
+    public String getCUI() {
+        return CUI;
+    }
+
+    public String getStatus() {
+        return Status;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public String getEdad() {
+        return Edad;
+    }
+
+    // Setters
+    public void setName(String Name) {
+        this.Name = Name;
+    }
+
+    public void setMaterno(String Materno) {
+        this.Materno = Materno;
+    }
+
+    public void setPaterno(String Paterno) {
+        this.Paterno = Paterno;
+    }
+
+    public void setGender(String Gender) {
+        this.Gender = Gender;
+    }
+
+    public void setCUI(String CUI) {
+        this.CUI = CUI;
+    }
+
+    public void setStatus(String Status) {
+        this.Status = Status;
+    }
+
+    public void setEmail(String Email) {
+        this.Email = Email;
+    }
+
+    public void setEdad(String Edad) {
+        this.Edad = Edad;
+    }
     
-    public Student(String nombre, String apellido) {
-        this.nombre = nombre;
-        this.apellido = apellido;
+    public String toString() {
+        return "Nombre: " + Name +
+               "Apellido Paterno: " + Paterno +
+               "Apellido Materno: " + Materno +
+               "Género: " + Gender +
+               "CUI: " + CUI +
+               "Estado: " + Status +
+               "Email: " + Email +
+               "Edad: " + Edad;
     }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public String getApellido() {
-        return apellido;
-    }
-
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
-    }
-    public void ingresarDatos(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Ingrese el nombre: ");
-        nombre=sc.nextLine();
-        System.out.println("Ingrese el Apellido: ");
-        apellido=sc.nextLine();
-    }
-
 }
